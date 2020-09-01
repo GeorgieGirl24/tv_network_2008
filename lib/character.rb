8da275a80df1be6ae8129975620dcbf430ff2b39
George Soderholm <georgesoderholm@gmail.com>
2020-09-01 09:10:23 -0600

    Add initialize method

---
+++ lib/character.rb
@@ -0,0 +1,8 @@
+class Character
+  attr_reader :name, :actor, :salary
+  def initialize(information)
+    @name = information[:name]
+    @actor = information[:actor]
+    @salary = information[:salary]
+  end
+end
