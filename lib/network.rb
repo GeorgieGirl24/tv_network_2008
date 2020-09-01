6e856cadf17cbd6985354d904571cf4e28b0ff1c
George Soderholm <georgesoderholm@gmail.com>
2020-09-01 10:49:21 -0600

    Add one method

--- lib/network.rb
+++ lib/network.rb
@@ -1,5 +1,6 @@
 class Network
-  attr_reader :name, :shows
+  attr_reader :shows
+  attr_accessor :name
   def initialize(name)
     @name = name
     @shows = []
@@ -8,4 +9,24 @@ class Network
   def add_show(show_name)
     @shows << show_name
   end
+
+  def main_characters
+    the_main_characters = @shows.map do |show|
+      show.characters.select do |character|
+        character.salary > 500_000
+      end
+    end.flatten
+    the_main_characters.select do |character|
+      character.name == character.name.upcase
+    end
+  end
+
+  def actors_by_show
+    @shows.group_by do |show|
+      show.characters.map do |character|
+        character.actor
+      end
+    end.invert
+  end
+
 end
